75f4b53950ff94d959d8724dd8741c89944fc94c
Shahariya Ahmmed siyam (siyuuu) <siyamislam1591@gmail.com>
2026-03-30 19:46:13 +0600

    Add files via upload

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -5,7 +5,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
 
 	return async function (event) {
-		// Check if the bot is in the inbox and anti inbox is enabled
+		// Anti-Inbox check
 		if (
 			global.GoatBot.config.antiInbox == true &&
 			(event.senderID == event.threadID || event.userID == event.senderID || event.isGroup == false) &&
@@ -15,10 +15,23 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 
 		const message = createFuncMessage(api, event);
 
+		// DB check/update
 		await handlerCheckDB(usersData, threadsData, event);
+
+		// Event handler load
 		const handlerChat = await handlerEvents(event, message);
 		if (!handlerChat)
 			return;
+
+		// Approval system
+		if(global.GoatBot.config?.approval){
+			const approvedtid = await globalData.get("approved", "data", {});
+			if (!approvedtid.approved) {
+				approvedtid.approved = [];
+				await globalData.set("approved", approvedtid, "data");
+			}
+			if (!approvedtid.approved.includes(event.threadID)) return;
+		}
 
 		const {
 			onAnyEvent, onFirstChat, onStart, onChat,
@@ -26,8 +39,9 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 			typ, presence, read_receipt
 		} = handlerChat;
 
-
+		// run any event
 		onAnyEvent();
+
 		switch (event.type) {
 			case "message":
 			case "message_reply":
@@ -37,29 +51,48 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 				onStart();
 				onReply();
 				break;
+
 			case "event":
 				handlerEvent();
 				onEvent();
 				break;
+
 			case "message_reaction":
 				onReaction();
+
+				const { delete: del, kick } = global.GoatBot.config?.reactBy || { delete: [], kick: [] };
+
+				// 🗑️ Delete message
+				if (del.includes(event.reaction)) {
+					if (event.senderID === api.getCurrentUserID()) {
+						if (global.GoatBot.config?.vipuser?.includes(event.userID)) {
+							api.unsendMessage(event.messageID);
+						}
+					}
+				}
+
+				// 👟 Kick user
+				if (kick.includes(event.reaction)) {
+					if (global.GoatBot.config?.vipuser?.includes(event.userID)) {
+						api.removeUserFromGroup(event.senderID, event.threadID, (err) => { 
+							if (err) return console.log(err); 
+						});
+					}
+				}
 				break;
+
 			case "typ":
 				typ();
 				break;
+
 			case "presence":
 				presence();
 				break;
+
 			case "read_receipt":
 				read_receipt();
 				break;
-			// case "friend_request_received":
-			// { /* code block */ }
-			// break;
 
-			// case "friend_request_cancel"
-			// { /* code block */ }
-			// break;
 			default:
 				break;
 		}
