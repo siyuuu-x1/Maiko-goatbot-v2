9dab03396fe7881a1ae7eae7e941680e3dcc9957
Shahariya Ahmmed siyam (siyuuu) <siyamislam1591@gmail.com>
2026-04-02 18:16:31 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * @author NTKhang & Modded by NeoKEX
+ * @author NTKhang 
  * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
  * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
  * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
